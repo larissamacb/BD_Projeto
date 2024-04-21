81fbcbd00c6e2f9e91cef34fdcd4ee1ffc8ceb98
Larissa <larissabmac@gmail.com>
2024-04-21 13:14:17 -0300

    Geral

---
+++ script.js
@@ -0,0 +1,10 @@
+
+const btnenviar = document.querySelector('#enviar')
+
+btnenviar.addEventListener('click', function(){
+    const nome = document.querySelector('#nome').value
+    const cnpj = document.querySelector('#cnpj').value
+    const nomepj = document.querySelector('#nomepj').value
+    const email = document.querySelector('#email').value
+    const acervotec = document.querySelector('#acervotec').value
+})
